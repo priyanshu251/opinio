626411fa5f4751a4ca689bf2bbdc86a0c0c49289
priyanshu251 <iampriyanshu251@gmail.com>
2023-06-06 15:36:54 +0530

    second commit

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kreviewTitle = TextStyle(
+    fontSize: 30, fontFamily: 'alkatra', fontWeight: FontWeight.normal);
+
+const kreviewDescription = TextStyle(fontSize: 17);
+
+const krating = TextStyle(fontSize: 15, fontWeight: FontWeight.bold);
--- lib/feedback_page.dart
+++ lib/feedback_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:practice2/constants.dart';
 
 class FeedbackPage extends StatefulWidget {
   FeedbackPage({Key? key, this.code}) : super(key: key);
@@ -9,6 +10,14 @@ class FeedbackPage extends StatefulWidget {
 }
 
 class _FeedbackPageState extends State<FeedbackPage> {
+  Color? disabledUpIconColor = Color.fromARGB(255, 135, 135, 135);
+  Color? disabledDownIconColor = Color.fromARGB(255, 135, 135, 135);
+  Color? abledUpIconColor = Colors.green[400];
+  Color? abledDownIconColor = Colors.red[400];
+  double? safetyRating = 3;
+  double? communicationRating = 3;
+  double? accuracyRating = 3;
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -16,25 +25,62 @@ class _FeedbackPageState extends State<FeedbackPage> {
     return Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ratingCard(
-                    screenHeight, 'Safety', 'how safe did softwares are?'),
-                ratingCard(screenHeight, 'Communication',
-                    'was there equal flow of communication'),
-                ratingCard(screenHeight, 'Accuracy',
-                    'did our softwares get your job done')
-              ],
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(9, 10, 10, 9),
+                child: const Text(
+                  'Leave a review',
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontFamily: "alkatra",
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ratingCard(
+                          screenHeight,
+                          'Safety',
+                          'How safe did softwares are ?',
+                          safetyRating!, (userSafetyRating) {
+                        setState(() {
+                          safetyRating = userSafetyRating;
+                        });
+                      }),
+                      ratingCard(
+                          screenHeight,
+                          'Communication',
+                          'Was there equal flow of communication',
+                          communicationRating!, (userCommunicationRating) {
+                        setState(() {
+                          communicationRating = userCommunicationRating;
+                        });
+                      }),
+                      ratingCard(
+                          screenHeight,
+                          'Accuracy',
+                          'Did our softwares get your job done accurately',
+                          accuracyRating!, (userAccuracyRating) {
+                        setState(() {
+                          accuracyRating = userAccuracyRating;
+                        });
+                      }),
+                      recommendationCard(screenHeight),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ));
   }
 
-  Container ratingCard(double screenHeight, String title, String description) {
+  Container recommendationCard(double screenHeight) {
     return Container(
-      height: screenHeight * (1 / 4),
       width: double.infinity,
       margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
       padding: const EdgeInsets.all(12),
@@ -51,10 +97,124 @@ class _FeedbackPageState extends State<FeedbackPage> {
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text(title),
-          Text(description),
-          const Icon(Icons.star),
+          const Text('Would you recommmend the Sigma IT softwares ?',
+              style: kreviewTitle),
+          SizedBox(
+            height: screenHeight * 0.01,
+          ),
+          Row(
+            children: [
+              IconButton(
+                icon:
+                    Icon(Icons.thumb_up, size: 40, color: disabledUpIconColor),
+                onPressed: () {
+                  setState(() {
+                    disabledUpIconColor = abledUpIconColor;
+                  });
+                },
+              ),
+              const SizedBox(
+                width: 65,
+              ),
+              IconButton(
+                icon: Icon(Icons.thumb_down,
+                    size: 40, color: disabledDownIconColor),
+                onPressed: () {
+                  setState(() {
+                    disabledDownIconColor = abledDownIconColor;
+                  });
+                },
+              ),
+            ],
+          ),
+          SizedBox(
+            height: screenHeight * 0.02,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container ratingCard(double screenHeight, String title, String description,
+      double rating, void Function(double) onChanged) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0xffDDDDDD),
+            blurRadius: 6.0,
+            spreadRadius: 2.0,
+            offset: Offset(0.0, 0.0),
+          )
+        ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(title, style: kreviewTitle),
+          SizedBox(
+            height: screenHeight * 0.003,
+          ),
+          Text(description, style: kreviewDescription),
+          SizedBox(
+            height: screenHeight * 0.02,
+          ),
+          RatingBar.builder(
+            itemSize: 50,
+            initialRating: 3,
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              switch (index) {
+                case 0:
+                  return const Icon(
+                    Icons.sentiment_very_dissatisfied,
+                    color: Colors.red,
+                    size: 25,
+                  );
+                case 1:
+                  return const Icon(
+                    Icons.sentiment_dissatisfied,
+                    color: Colors.redAccent,
+                  );
+                case 2:
+                  return const Icon(
+                    Icons.sentiment_neutral,
+                    color: Colors.amber,
+                  );
+                case 3:
+                  return const Icon(
+                    Icons.sentiment_satisfied,
+                    color: Colors.lightGreen,
+                  );
+                case 4:
+                  return const Icon(
+                    Icons.sentiment_very_satisfied,
+                    color: Colors.green,
+                  );
+                default:
+                  return const Icon(
+                    Icons.sentiment_neutral,
+                    color: Colors.amber,
+                  );
+              }
+            },
+            onRatingUpdate: (rating) {
+              onChanged(rating);
+            },
+          ),
+          SizedBox(
+            height: screenHeight * 0.02,
+          ),
+          Text('Your rating: $rating', style: krating)
         ],
       ),
     );
